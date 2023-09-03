5d14dbc1746c5f9258b05839c352a00fd29d4467
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-09-03 22:49:58 +0300

    init:commit add image

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,26 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-      home:Scaffold(
-        appBar: AppBar(
-          // leading: TextButton(onPressed: (){} , child: Text("Click")),
-          // leadingWidth: 250,
-          title: Center(child: Text("My App") ),
-          backgroundColor: Colors.amber,
-        ),
-        body: Column(
-          children:[
+    home: Scaffold(
+      appBar: AppBar(
+        // leading: TextButton(onPressed: (){} , child: Text("Click")),
+        // leadingWidth: 250,
+        title: Center(child: Text("My App")),
+        backgroundColor: Colors.amber,
+      ),
+      body: Column(
+        children: [
           Text("Welcome Flutter"),
           // Image(image: NetworkImage("https://plus.unsplash.com/premium_photo-1674831527054-f8d0a89e368d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80")),
-          Image.asset('lib/assets/fail.png')
-          ],
-        ),
-        backgroundColor: Colors.cyan[400],
+          Center(
+            child: Image.asset(
+              'lib/assets/fail.png',
+              // fit: BoxFit.fill,
+            ),
+          )
+        ],
       ),
-  )
-  );
+      backgroundColor: Colors.cyan[400],
+    ),
+  ));
 }
