350f549e410b9f6e75feb49938940fe4188976c1
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-09-11 00:27:07 +0300

    init:commit add code

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        // leading: TextButton(onPressed: (){} , child: Text("Click")),
-        // leadingWidth: 250,
-        title: Center(child: Text("My App")),
-        backgroundColor: Colors.amber,
-      ),
-      body: Column(
-        children: [
-          Text("Welcome Flutter"),
-          // Image(image: NetworkImage("https://i.pinimg.com/originals/d4/8a/ac/d48aacdb727b0aaf15fbab89d4723a40.gif")),
-          Center(
-            // child: Image.asset(
-            //   'lib/assets/141414.gif',
-            //   // fit: BoxFit.fill,
-            // ),
-            child: Image(image:  AssetImage('lib/assets/141414.gif'))
-          )
-        ],
-      ),
-      backgroundColor: Colors.cyan[400],
-    ),
-  ));
+  runApp(First());
 }
+
+class First extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              // alignment: Alignment.bottomCenter,
+              padding: const EdgeInsets.all(100),
+              decoration: BoxDecoration(
+                color : Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(100)
+                )
+              ),
+              // color: Colors.white,
+              width: 400,
+              height: 400,
+              child: const Text("Mohammed"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Image(image: NetworkImage("https://i.pinimg.com/originals/d4/8a/ac/d48aacdb727b0aaf15fbab89d4723a40.gif")),
+
+// child: Image.asset(
+//   'lib/assets/141414.gif',
+//   // fit: BoxFit.fill,
+// ),
+
+
+//2 
+//  home: Scaffold(
+//       appBar: AppBar(
+//         // leading: TextButton(onPressed: () {}, child: Text("Click")),
+//         // leadingWidth: 250,
+//         title: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Text(
+//             "My App",
+//           ),
+//         ),
+//         backgroundColor: Colors.amber,
+//       ),
+//       body: Center(
+//         child: Image(
+//           image: AssetImage('lib/assets/141414.gif'),
+//         ),
+//       ),
+//       backgroundColor: Colors.cyan[400],
+//     ),
